a7b3b5c72ab6ce91b56d3cf06d803945dc748068
Amitai Blickstein <blickstein@gmail.com>
2021-07-29 15:18:33 -0400

    Adds InitialViewController, resolves IB warnings

--- JWPlayerKit-IssueReproduction/SwiftViewController.swift
+++ JWPlayerKit-IssueReproduction/SwiftViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 import JWPlayerKit
 
 class SwiftViewController: UIViewController {
+    @IBOutlet var playerContainerView: UIView!
     private var playerView = JWPlayerView()
     
     override func viewDidLoad() {
