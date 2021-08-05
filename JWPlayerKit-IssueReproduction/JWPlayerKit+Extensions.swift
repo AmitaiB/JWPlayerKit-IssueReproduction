15c98b1d3d6da8387bd7cc9135cd963b071778c5
Amitai Blickstein <blickstein@gmail.com>
2021-08-05 18:09:55 -0400

    JWxxxReasons conforms to CustomStringConvertible

--- JWPlayerKit-IssueReproduction/JWPlayerKit+Extensions.swift
+++ JWPlayerKit-IssueReproduction/JWPlayerKit+Extensions.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+import JWPlayerKit
+
+extension JWPauseReason: CustomStringConvertible {
+    public var description: String {
+        switch self {
+            case .clickthrough: return "clickthrough"
+            case .external:     return "external"
+            case .interaction:  return "interaction"
+            case .unknown:      return "unknown"
+            @unknown default:   return "unknown"
+                
+        }
+    }
+}
+
+extension JWPlayReason: CustomStringConvertible {
+    public var description: String {
+        switch self {
+            case .external:     return "external"
+            case .interaction:  return "interaction"
+            case .playlist:     return "playlist"
+            case .viewable:     return "viewable"
+            case .repeatContent: return "repeatContent"
+            case .relatedInteraction: return "relatedInteraction"
+            case .relatedAuto:  return "relatedAuto"
+            case .unknown:      return "unknown"
+            @unknown default:   return "unknown"
+                
+        }
+    }
+}
+
+extension JWIdleReason: CustomStringConvertible {
+    public var description: String {
+        switch self {
+            case .complete: return "complete"
+            case .idle:     return "idle"
+            case .unknown:  return "unknown"
+            @unknown default: return "unknown"
+        }
+    }
+}
